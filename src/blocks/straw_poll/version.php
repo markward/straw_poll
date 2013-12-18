2d43e1ab91f7630bd61a7b63bbe8b9afb7f647ed
Mark Ward <mark.ward@gmail.com>
2013-12-18 14:21:52 +0000

    Incremented version number to trigger permission install

--- src/blocks/straw_poll/version.php
+++ src/blocks/straw_poll/version.php
@@ -1,7 +1,7 @@
 <?php
-    $plugin->version = 2013032000;  // YYYYMMDDHH (year, month, day, 24-hr time)
+    $plugin->version = 2013121800;  // YYYYMMDDHH (year, month, day, 24-hr time)
     $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
-	$plugin->release = '0.9'; //plugin release
+	$plugin->release = '0.91'; //plugin release
 	$plugin->maturity   = MATURITY_BETA;
 	$plugin->component = 'block_straw_poll'; // Full name of the plugin (used for diagnostics)
 ?>
